2a73f9febddb8ad1b0881ad8bb52e2c4ab51de3a
karadasmurat <karadasmurat@gmail.com>
2023-04-25 20:34:26 +0300

    express, ejs

--- helloexpress/app.js
+++ helloexpress/app.js
@@ -1,4 +1,13 @@
 /*
+Express is a minimal and flexible Node.js web application framework.
+
+Application
+The app object conventionally denotes the Express application.
+Create it by calling the top - level express() function exported by the Express module:
+
+    var express = require('express')
+    var app = express()
+
 
 Basic routing
 Routing refers to determining how an application responds to a client request to a particular endpoint, 
@@ -14,33 +23,84 @@ Where:
  • HANDLER is the function executed when the route is matched.
 
 */
-const express = require('express')
-const app = express()
+const express = require('express');
 
-app.use(express.static("public"));
+// The app object conventionally denotes the Express application. 
+const app = express();
+
+// The Path module provides a way of working with directories and file paths.
+const path = require('path');
+
+const todojs = require('./public/js/todo.js');
+
+// Set the "views" directory, relative to this file
+// If you run the express app from another directory, it’s safer to use the absolute path of the directory 
+app.set('views', path.join(__dirname, '/views'));
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Serving static files in Express
+// here, we use a top-level directory named "public"
+// app.use(express.static("public"));
+// If you run the express app from another directory, it’s safer to use the absolute path of the directory that you want to serve:
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3001
-// Sample todo data
-const todos = [{
-        id: 1,
-        task: 'Buy groceries'
-    },
-    {
-        id: 2,
-        task: 'Do laundry'
-    },
-    {
-        id: 3,
-        task: 'Walk the dog'
-    }
-];
 
+// Sample todo data
+// const todos = [{
+//         id: 1,
+//         task: 'Buy groceries'
+//     },
+//     {
+//         id: 2,
+//         task: 'Do laundry'
+//     },
+//     {
+//         id: 3,
+//         task: 'Walk the dog'
+//     }
+// ];
+
+// In Node.js, we can directly require a JSON file and it will be parsed into a JavaScript object. 
+const todos_v0 = require('./data/todos.json');
+// process each item, add another property, and return. (map)
+const todos = todos_v0.map(todo => {
+    let status = "NA";
+    if (todo.completed) {
+        status = "completed";
+    } else if (todojs.passedDueDate(todo.due_date)) {
+        status = "missed";
+    } else {
+        status = "on_track";
+    }
+    const daysRemaining = todojs.getRemainingDays(todo.due_date);
+    return {
+        ...todo,
+        status,
+        daysRemaining
+    }
+});
+
+// Respond to GET request on the root route (/):
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    //res.send('Hello World!')
+
+    // send the rendered view to the client
+    res.render('home.ejs');
 })
 
 app.get("/todos", (request, response) => {
     response.status(200).json(todos);
+});
+
+// pass a local variable (random die roll) to the view: {varName: value}
+// shortcut: {varName}
+app.get("/ejstodos", (request, response) => {
+    response.render('todo.ejs', {
+        todos
+    });
 });
 
 // Route parameters are named URL segments
@@ -73,6 +133,15 @@ app.get('/api/users', function (req, res) {
     });
 });
 
+// pass a local variable (random die roll) to the view: {varName: value}
+// shortcut: {varName}
+app.get("/rand", (request, response) => {
+    const dieRoll = Math.floor(Math.random() * 6) + 1
+    response.render('rand.ejs', {
+        die: dieRoll
+    });
+});
+
 // Respond to POST request on the root route (/):
 app.post('/', (req, res) => {
     res.send('Got a POST request')
@@ -83,6 +152,9 @@ app.put('/user', (req, res) => {
     res.send('Got a PUT request at /user')
 })
 
+
+// bind and listen for connections on the specified host and port.
+// identical to Node’s http.Server.listen()
 app.listen(port, () => {
     console.log(`helloexpress app listening on port ${port}`)
 })

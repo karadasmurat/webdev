55392d1247a7861a93fc6b6b168daeaae5b7f545
karadasmurat <karadasmurat@gmail.com>
2023-06-19 01:19:54 +0300

    colorbox exercise

---
+++ react/exercises/colorbox/src/App.tsx
@@ -0,0 +1,12 @@
+import Colorbox from "./components/Colorbox";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">hello, there</p>
+      <Colorbox />
+    </>
+  );
+}
+
+export default App;
---
+++ react/exercises/colorbox/src/components/Colorbox.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function Colorbox() {
+  const [selectedColor, setSelectedColor] = useState("");
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "200px",
+          height: "200px",
+          border: "1px solid black",
+          borderRadius: "5px",
+          backgroundColor: selectedColor,
+          margin: "10px 0px",
+        }}
+      >
+        {selectedColor ? selectedColor : "Empty value"}
+      </div>
+
+      <input
+        type="text"
+        name="color"
+        id="color"
+        value={selectedColor}
+        onChange={(e) => setSelectedColor(e.target.value)}
+        placeholder="Add color name"
+      />
+    </>
+  );
+}
